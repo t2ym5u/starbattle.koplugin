38bbf9b056b6497ac4fb7217840b1250358c977b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:18:38 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,3 +1,6 @@
 return {
     ["%1\xC3\x97%2 \xC2\xB7 %3\xE2\x98\x85/row \xC2\xB7 Stars: %4/%5"] = { fr = "%1×%2 · %3★/ligne · Étoiles : %4/%5" },
+    ["%1\xC3\x97%2 (%3\xE2\x98\x85)"] = { fr = "%1×%2 (%3★)" },
+    ["Place stars with one per row/col/region"] = { fr = "Placez des étoiles, une par ligne/colonne/région" },
+    ["Star Battle"] = { fr = "Star Battle" },
 }
