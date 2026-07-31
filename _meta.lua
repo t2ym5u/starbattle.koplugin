5ae04cd42ef17771f88dd26b6b7112d754eb5852
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-31 17:48:07 +0200

    chore: bump version to 1.1.13

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Star Battle"),
     description = _("Place stars with one per row/col/region"),
-    version     = "1.1.12",
+    version     = "1.1.13",
 }
